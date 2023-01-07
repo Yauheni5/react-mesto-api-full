658df8e5abf5952b723223f605d91e88f4103a49
Yauheni5 <yauheni5@github.com>
2023-01-07 13:12:05 +0300

    Revert "fix"
    This reverts commit 95399675d68e2d91a92f9c07d0c2c0957f763755.

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors({
-  Origin: 'https://yauheni.nomoredomains.club/',
+  Origin: 'https://api.yauheni.nomoredomains.club/',
 }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
