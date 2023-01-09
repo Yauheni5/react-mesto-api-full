207b744206dac9185d3a71ca687415b44952a2f5
Yauheni5 <yauheni5@github.com>
2023-01-09 18:18:41 +0300

    fix: src mongodb, add build gitignore

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
